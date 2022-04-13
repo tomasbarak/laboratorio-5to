e874955d7197392ae96a9ab81ab8979b29f69806
Tomás Barak <70740123+tomasbarak@users.noreply.github.com>
2022-04-13 10:54:43 -0300

    Create Main.java

---
+++ evaluacion13-04/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Cantante cantante = new Cantante();
+        cantante.setNombre("Nombre de ejemplo");
+        System.out.println(cantante.getNombre());
+    }
+}
